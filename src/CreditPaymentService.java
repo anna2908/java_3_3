bf6344232ab17b3becf8eaff34b1faf6bf1117a0
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2022-11-05 12:22:44 +0300

    Add project

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,10 @@
+public class CreditPaymentService
+{
+    public int calculate(int sum, int year, double percent)
+    {
+        double m = percent / 100 / 12;
+        int n = 12 * year;
+        double k = m*Math.pow(1+m, n)/(Math.pow(1+m,n)-1);
+        return (int) (sum*k);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        CreditPaymentService service = new CreditPaymentService();
+        System.out.println(service.calculate(1_000_000, 1, 9.99F));
+        System.out.println(service.calculate(1_000_000, 2, 9.99F));
+        System.out.println(service.calculate(1_000_000, 3, 9.99F));
+
+
+    }
+}
