638dd9b9ddb78747db20119a8e4284e0089352bb
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2022-11-09 18:15:12 +0300

    Format

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,8 @@
-public class CreditPaymentService
-{
-    public int calculate(int sum, int year, double percent)
-    {
+public class CreditPaymentService {
+    public int calculate(int sum, int year, double percent) {
         double m = percent / 100 / 12;
         int n = 12 * year;
-        double k = m*Math.pow(1+m, n)/(Math.pow(1+m,n)-1);
-        return (int) (sum*k);
+        double k = m * Math.pow(1 + m, n) / (Math.pow(1 + m, n) - 1);
+        return (int) (sum * k);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         System.out.println(service.calculate(1_000_000, 1, 9.99F));
         System.out.println(service.calculate(1_000_000, 2, 9.99F));
